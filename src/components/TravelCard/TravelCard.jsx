f370b65fe527c6c853178da7b359915dd5689fea
Prakash Sakari <sakariprakash@gmail.com>
2022-09-27 21:55:24 +0530

    added wishlist button

--- src/components/TravelCard/TravelCard.jsx
+++ src/components/TravelCard/TravelCard.jsx
@@ -1,7 +1,8 @@
 import { useNavigate } from "react-router-dom";
+import { checkHotelInWishlist } from "../../utilities"
 import "./TravelCard.css";
 
-export const TravelCard = ({hotel}) => {
+export const TravelCard = ({ hotel }) => {
 
     const { id, name, image, address, country, rating, price } = hotel;
 
@@ -11,20 +12,34 @@ export const TravelCard = ({hotel}) => {
         navigate(`/hotel/${id}`)
     }
 
+    const handleWishlistClick = () => {
+        console.log("clicked");
+    }
+
     return (
-        <div className="travelcard-container shadow cursor-pointer" onClick={handleSelectHotelClick}>
-            <img className="img" src={image} alt=""/>
-            <div className="travelcard-details d-flex align-center">
-                <span className="location">{address}, {country}</span>
-                <span className="rating d-flex align-center">
-                    <span class="material-icons-outlined">
-                        star
+        <div className="relative">
+            <div className="travelcard-container shadow cursor-pointer " onClick={handleSelectHotelClick}>
+                <img className="img" src={image} alt="" />
+                <div className="travelcard-details d-flex align-center">
+                    <span className="location">{address}, {country}</span>
+                    <span className="rating d-flex align-center">
+                        <span class="material-icons-outlined">
+                            star
+                        </span>
+                        {rating}
                     </span>
-                    {rating}
-                </span>
+                </div>
+                <p className="hotel-name">{name}</p>
+                <p className="price-details"><span className="price">₹{price}</span> night</p>
+            </div >
+            <div className="wishlist">
+                <button className={`button btn-wishlist d-flex align-center justify-center absolute`} onClick={handleWishlistClick}>
+                    <span class="material-icons favorite cursor">
+                        favorite
+                    </span>
+                </button>
             </div>
-            <p className="hotel-name">{name}</p>
-            <p className="price-details"><span className="price">₹{price}</span> night</p>
         </div>
+
     )
 }
