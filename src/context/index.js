addc6cbddf2dbc1e088bef794c53a51bb260dace
Prakash Sakari <sakariprakash@gmail.com>
2022-09-28 11:52:04 +0530

    added hotels to wishlist

--- src/context/index.js
+++ src/context/index.js
@@ -1,3 +1,4 @@
 export { useAuth, AuthProvider } from "./auth-context";
 export { useCategory, CategoryProvider } from "./category-context";
 export { useFilter, FilterProvider } from "./filter-context";
+export { useWishlist, WishlistProvider } from "./wishlist-context";
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { CategoryProvider, AuthProvider, FilterProvider } from './context';
+import { CategoryProvider, AuthProvider, FilterProvider, WishlistProvider } from './context';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,7 +12,9 @@ root.render(
       <AuthProvider>
         <CategoryProvider>
           <FilterProvider>
-            <App />
+            <WishlistProvider>
+              <App />
+            </WishlistProvider>
           </FilterProvider>
         </CategoryProvider>
       </AuthProvider>
